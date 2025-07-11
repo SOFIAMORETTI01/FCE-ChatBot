dbb38bce6f02f29efe92812bd7a1c9376476caa6
SOFIAMORETTI01 <166921960+SOFIAMORETTI01@users.noreply.github.com>
2025-07-10 23:07:58 -0300

    Update Script_FCE_ChatBot.py

--- Script_FCE_ChatBot.py
+++ Script_FCE_ChatBot.py
@@ -51,9 +51,7 @@ def normalizar(texto):
 # Cargar el archivo de materias, limpiar texto y agregar columnas normalizadas
 @st.cache_data
 def cargar_datos():
-    import csv
-    import pandas as pd
-      df = pd.read_csv("Materias_BOT.csv", sep=";", engine="python", encoding="latin1", lineterminator="\n")
+    df = pd.read_csv("Materias_BOT.csv", sep=";", engine="python", encoding="latin1", lineterminator="\n")
 
     df.columns = df.columns.str.strip()
 
